3403f085607f86970c7c05324f24ad05e4976286
happycb <happycb@hsgold>
2019-11-26 22:35:38 +0800

    first commit

---
+++ docs/precache-manifest.b30e0ff6a4b1d761aa9af24dac00ad0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a9869895a1f9a07b60830e134c71bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d4c0072da4cd6ed5f37f",
+    "url": "./static/css/2.3c3f7bee.chunk.css"
+  },
+  {
+    "revision": "616d39b072c40abc4c39",
+    "url": "./static/css/main.5d737727.chunk.css"
+  },
+  {
+    "revision": "d4c0072da4cd6ed5f37f",
+    "url": "./static/js/2.90e3120b.chunk.js"
+  },
+  {
+    "revision": "616d39b072c40abc4c39",
+    "url": "./static/js/main.132a811e.chunk.js"
+  },
+  {
+    "revision": "4539646fd9f7b44040f9",
+    "url": "./static/js/runtime~main.646c5149.js"
+  },
+  {
+    "revision": "02f691ae6d3ec014ebf43b2a5cbf09e2",
+    "url": "./static/media/logo.02f691ae.png"
+  }
+]);
